f993eef353a9dbb63450d49313813864565a4e16
Chris Qiang <chris@christianto.net>
2024-11-03 09:10:04 +0700

    docs: add documentations for the foreign exchange

--- src/api/foreign_exchange/mod.rs
+++ src/api/foreign_exchange/mod.rs
@@ -1,3 +1,73 @@
+//! # Foreign Exchange API Module
+//!
+//! This module provides access to the Visa Foreign Exchange API, allowing users
+//! to fetch real-time and indicative exchange rates for various currency pairs.
+//! The module includes structures and methods to create and send requests, as
+//! well as to handle responses from the API.
+//!
+//! ## Overview
+//!
+//! Visa supports both regular and enhanced versions of the Foreign Exchange
+//! Rate API. Currently, this module implements only the regular version of the
+//! API.
+//!
+//! - The regular version does not require the use of Message Level Encryption
+//!   (MLE).
+//! - The enhanced version requires Message Level Encryption (MLE) for added
+//!   security.
+//!
+//! The [`ForeignExchange`] struct is the main entry point for interacting with
+//! the Visa Foreign Exchange API. It provides methods to create requests for
+//! different rate products, such as card-based, account-based, bank, and wallet
+//! rates.
+//!
+//! ## Example
+//!
+//! Below is an example of how to create a request to fetch foreign exchange
+//! rates using the [`ForeignExchange`] struct:
+//!
+//! ```no_run
+//! use visa_sdk::client::VisaClient;
+//! use visa_sdk::api::foreign_exchange::ForeignExchange;
+//! use visa_sdk::api::foreign_exchange::models::{FXRequestAorBBuilder, FXRequestBankOrWalletBuilder};
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let client = VisaClient::new("api_key", "secret_key");
+//!     let forex = ForeignExchange::new(client);
+//!
+//!     // Example for FXRequestAorB
+//!     let payload_a_or_b = FXRequestAorBBuilder::default()
+//!         .source_currency_code("USD".to_string())
+//!         .destination_currency_code("GBP".to_string())
+//!         .source_amount("100.55".to_string())
+//!         .build()
+//!         .expect("Failed to build FXRequestAorB");
+//!     let response_a_or_b = forex.get_a_or_b(payload_a_or_b).await;
+//!     println!("{:?}", response_a_or_b);
+//!
+//!     // Example for FXRequestBankOrWallet
+//!     let payload_bank_or_wallet = FXRequestBankOrWalletBuilder::default()
+//!         .source_currency_code("USD".to_string())
+//!         .destination_currency_code("GBP".to_string())
+//!         .initiating_party_id(1002)
+//!         .source_amount(Some(100.55))
+//!         .quote_id_required(Some(true))
+//!         .build()
+//!         .expect("Failed to build FXRequestBankOrWallet");
+//!     let response_bank_or_wallet = forex.get_bank_or_wallet(payload_bank_or_wallet).await;
+//!     println!("{:?}", response_bank_or_wallet);
+//! }
+//! ```
+//!
+//! ## Documentation
+//!
+//! For more details, consult the Visa API documentation:
+//! - [Visa Foreign Exchange
+//!   API](https://developer.visa.com/capabilities/foreign_exchange)
+//! - [Visa Foreign Exchange API
+//!   Reference](https://developer.visa.com/capabilities/foreign_exchange/reference)
+
 mod regular;
 
 pub use regular::*;
--- src/api/foreign_exchange/regular.rs
+++ src/api/foreign_exchange/regular.rs
@@ -9,7 +9,19 @@ use reqwest::{Method, Request};
 use serde_json::json;
 use url::Url;
 
-/// Foreign Exchange API
+/// [`ForeignExchange`] struct provides methods to interact with the Visa
+/// Foreign Exchange API.
+///
+/// This struct allows you to create requests to fetch foreign exchange rates
+/// using different payloads. It requires a `VisaClient` instance to be
+/// initialized.
+///
+/// # Example
+///
+/// ```
+/// let client = VisaClient::new(api_key, secret_key);
+/// let forex = ForeignExchange::new(client);
+/// ```
 ///
 /// Guide:
 /// - <https://developer.visa.com/capabilities/foreign_exchange>
@@ -28,11 +40,45 @@ where
 {
     const URL: &'static str = "/forexrates/v2/foreignexchangerates";
 
+    /// Creates a new instance of `ForeignExchange`.
+    ///
+    /// # Arguments
+    ///
+    /// - `client`: A `VisaClient` instance. Does not need to have Message Level
+    ///   Encryption enabled.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let client = VisaClient::new(api_key, secret_key);
+    /// let forex = ForeignExchange::new(client);
+    /// ```
     pub fn new(client: VisaClient<MLE>) -> Self {
         let url = client.get_base_url().join(Self::URL).unwrap();
         ForeignExchange { client, url }
     }
 
+    /// Fetches foreign exchange rates using `FXRequestAorB` payload.
+    ///
+    /// This function returns indicative daily rates for transactions with card
+    /// or bank-account as the payment instrument.
+    ///
+    /// # Arguments
+    ///
+    /// - `payload`: An instance of `FXRequestAorB`.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// let payload = FXRequestAorBBuilder::default()
+    ///     .source_currency_code("USD".to_string())
+    ///     .destination_currency_code("GBP".to_string())
+    ///     .source_amount("100.55".to_string())
+    ///     .build()
+    ///     .expect("Failed to build FXRequestAorB");
+    /// let response = forex.get_a_or_b(payload).await.expect("Failed to get response");
+    /// println!("{:?}", response);
+    /// ```
     pub async fn get_a_or_b(&self, payload: FXRequestAorB) -> Result<FXResponseAorB> {
         let mut request = Request::new(Method::GET, self.url.clone());
         request
@@ -41,6 +87,30 @@ where
         let response = self.client.execute_request(request).await?;
         Ok(response.json::<FXResponseAorB>().await?)
     }
+
+    /// Fetches foreign exchange rates using `FXRequestBankOrWallet` payload.
+    ///
+    /// This function returns real-time rates for transactions with bank-account
+    /// or wallet as the payment instrument.
+    ///
+    /// # Arguments
+    ///
+    /// - `payload`: An instance of `FXRequestBankOrWallet`.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// let payload = FXRequestBankOrWalletBuilder::default()
+    ///     .source_currency_code("USD".to_string())
+    ///     .destination_currency_code("GBP".to_string())
+    ///     .initiating_party_id(1002)
+    ///     .source_amount(Some(100.55))
+    ///     .quote_id_required(Some(true))
+    ///     .build()
+    ///     .expect("Failed to build FXRequestBankOrWallet");
+    /// let response = forex.get_bank_or_wallet(payload).await.expect("Failed to get response");
+    /// println!("{:?}", response);
+    /// ```
     pub async fn get_bank_or_wallet(
         &self,
         payload: FXRequestBankOrWallet,
