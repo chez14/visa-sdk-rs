333d71119d7096e4329ddb2208b81252c103ce43
Chris Qiang <chris@christianto.net>
2024-11-03 10:40:17 +0700

    docs: add mini documentation for api levels

--- src/client/models/api_level.rs
+++ src/client/models/api_level.rs
@@ -1,7 +1,16 @@
+/// Represents the API level for the application, which determines the URL to be
+/// used.
+///
+/// ## Documentations
+/// - <https://developer.visa.com/pages/going-live>
+/// - <https://developer.visa.com/pages/visa-developer-pki>
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum ApiLevel {
+    /// Use the sandbox base URL for testing and development.
     #[default]
     Sandbox,
+    /// Use the certification base URL for pre-production testing.
     Certification,
+    /// Use the production base URL for live operations.
     Production,
 }
